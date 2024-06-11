34a82b7283a44f834c7acad2cf1639ca52f1de70
Chen Run Nan <129401246+RunnanChenUSTC@users.noreply.github.com>
2024-06-11 23:37:26 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
         {children}
         {serverConfig?.isVercel && (
           <>
-            <SpeedInsights />
+{/*             <SpeedInsights /> */}
           </>
         )}
       </body>
